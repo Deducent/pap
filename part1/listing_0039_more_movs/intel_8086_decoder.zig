4e3b23ba3aaa17094abae4875f14043792599a28
Deducent <honghaho@web.de>
2024-10-28 19:03:49 +0100

    intermediate for mov with displacement

--- part1/listing_0039_more_movs/intel_8086_decoder.zig
+++ part1/listing_0039_more_movs/intel_8086_decoder.zig
@@ -8,6 +8,7 @@ pub fn main() !void {
     var buffer: [1024]u8 = [_]u8{undefined} ** 1024;
     const file_size = try reader.readAll(&buffer);
     const bytes = buffer[0..file_size];
+    std.debug.print("bytes: {b}\n", .{bytes});
 
     const test_file = try std.fs.cwd().createFile("test.asm", .{ .read = true });
     defer test_file.close();
@@ -50,6 +51,10 @@ fn mov_immediate(bytes: []u8, writer: std.fs.File.Writer) !void {
 fn mov_register_to_register(bytes: []u8, writer: std.fs.File.Writer) !void {
     const byte1 = bytes[i];
     const byte2 = bytes[i + 1];
+
+    // const disp_l: u8 = undefined;
+    // const disp_h: u16 = undefined;
+
     const d: u1 = if ((byte1 & 0b000000_1_0) > 0) 1 else 0; // d = 0 REG-Field is source operand | d = 1 REG-Field is destination operand
     const w: u1 = if ((byte1 & 0b0000000_1) > 0) 1 else 0;
 
@@ -60,20 +65,60 @@ fn mov_register_to_register(bytes: []u8, writer: std.fs.File.Writer) !void {
         0b11_000000 => 0b11,
         else => unreachable,
     };
-
-    _ = .{mod};
+    std.debug.print("mod: {b}\n", .{mod});
 
     var reg: [2]u8 = undefined;
-    copy_register_name(byte2 >> 3, w, &reg);
+    var r_m: [17]u8 = [_]u8{undefined} ** 17;
 
-    var r_m: [2]u8 = undefined;
-    copy_register_name(byte2, w, &r_m);
+    if (mod == 0b11) {
+        copy_register_name(byte2 >> 3, w, &reg);
+        copy_register_name(byte2, w, r_m[0..2]);
+        i += 2;
+    } else {
+        std.mem.copyForwards(u8, &r_m, switch (byte2 & 0b00_000_111) {
+            0b00_000_000 => "bx + si",
+            0b00_000_001 => "bx + di",
+            0b00_000_010 => "bp + si",
+            0b00_000_011 => "bp + di",
+            0b00_000_100 => "si",
+            0b00_000_101 => "di",
+            0b00_000_110 => if (mod == 0b00) "75" else "bp", // keine ahnung gerade disp_h
+            0b00_000_111 => "bx",
+            else => unreachable,
+        });
+        std.debug.print("rm: {s}", .{r_m});
+
+        // _ = switch (mod) {
+        //     0b01 => try std.fmt.bufPrint(&r_m, "[{s} + {d}]\n", .{ r_m, disp_l }),
+        //     0b10 => try std.fmt.bufPrint(&r_m, "[{s} + {d}]\n", .{ r_m, (disp_h << 8) | disp_l }),
+        //     0b00 => try std.fmt.bufPrint(&r_m, "[{s}]\n", .{r_m}),
+        //     else => unreachable,
+        // };
+
+        // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        // const allocator = gpa.allocator();
+
+        // r_m = switch (mod) {
+        //     0b01 => try std.fmt.allocPrint(allocator, "[{s} + {d}]\n", .{ r_m, disp_l }),
+        //     0b10 => try std.fmt.allocPrint(allocator, "[{s} + {d}]\n", .{ r_m, (disp_h << 8) | disp_l }),
+        //     0b00 => try std.fmt.allocPrint(allocator, "[{s}]\n", .{r_m}),
+        //     else => unreachable,
+        // };
+        // defer allocator.free();
+
+        switch (mod) {
+            0b01 => i += 3,
+            0b10 => i += 4,
+            0b00 => i += 2,
+            else => unreachable,
+        }
+    }
+
     if (d == 0) {
         try writer.print("{s} {s}, {s}\n", .{ "mov", r_m, reg });
     } else {
         try writer.print("{s} {s}, {s}\n", .{ "mov", reg, r_m });
     }
-    i += 2;
 }
 
 fn copy_register_name(addr: u8, w: u1, dest: *[2]u8) void {
