bc81b7cdd5a7b48b2c50b577b805df73a1eb7434
Deducent <honghaho@web.de>
2024-12-27 19:42:06 +0100

    finish simulating memory and testing the disassembly

--- part1/src/main.zig
+++ part1/src/main.zig
@@ -81,7 +81,6 @@ const cpu_regs = struct {
     }
 
     fn eval_effective_address_calc(self: *cpu_regs, effective_address_calc: []const u8) !usize {
-        var index: usize = 0;
         var begin: usize = 0;
         for (effective_address_calc, 0..) |char, i| {
             if (char == '[') {
@@ -91,6 +90,7 @@ const cpu_regs = struct {
         }
         var substrings = std.mem.splitSequence(u8, effective_address_calc[begin .. effective_address_calc.len - 1], " + ");
 
+        var index: usize = 0;
         while (substrings.next()) |chunk| {
             var is_numeric: bool = false;
             for (chunk) |char| {
